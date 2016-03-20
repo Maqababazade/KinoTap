a3603eb6191c2a274795efd70b775b77e1bb30a5
Heyder Shukurov <Heyder Shukurov>
2016-03-20 16:06:50 +0400

    Kinopedia and img icons

---
+++ kinopedia/kinopedia.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kinotap</title>
+
+	<link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="style/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" type="text/css" href="style/kinopediacustom.css">
+</head>
+<body>
+
+<div class="container">
+<div class="main">
+
+	<div class="header">
+
+		
+		
+	<!-- Header  -->
+				<div class="htop">
+	<!-- Image -->
+					<div class="col-md-2 img">
+						<img src="../img/logo.png">
+					</div>
+	<!-- Search Box -->
+					<div class="col-md-7 searchBox">
+						<div class="searchBoxlilk">
+					<form>
+						<input type="search" placeholder="Search...">
+						<select>
+							<option>Section 1</option>
+							<option>Section 2</option>
+							<option>Section 3</option>
+						</select>
+						<button class="buttonK btn btn-success" type="submit"><i class="fa fa-search"></i></button>
+					</form>
+						</div>
+					</div>
+					<div class="rightbar col-md-3">
+						<div class="btn-group topbt" role="group" aria-label="...">
+						  <button type="button" class="btn btn-default leff">Daxil ol</button>
+						  <button type="button" class="btn btn-default midd mi">
+						  	<i class="fa fa-twitter"></i>
+						  </button>
+						  <button type="button" class="btn btn-default midd ri">
+						  	<i class="fa fa-facebook"></i>
+						  </button>
+						</div>
+						<div class="btn-group topbt action" role="group" aria-label="...">
+						  <button type="button" class="btn btn-default middt"><p>AZ</p></button>
+						  <button type="button" class="btn btn-default middt">
+						  	<p>RU</p>
+						  </button>
+						  <button type="button" class="btn btn-default middt">
+						  	<p>EN</p>
+						  </button>
+						</div>
+
+					</div>
+				</div>
+			
+		
+
+			<!--   -->
+				<div class="col-md-12 navbarm">
+					
+					    <div><li>ANA SEHIFE</li></div>
+					    <div><li>SEANSLAR</li></div>
+					    <div><li>TEZLIKLE</li></div>
+					    <div><li>KINOPEDIA</li></div>
+					    <div><li>KINOTEATRLAR</li></div>
+					    <div><li>XEBERLER</li></div>
+					
+				</div>
+			<!--   -->
+	</div>
+
+<!-- Middle Section Starts HERE -->
+<div class="midSection">
+
+	<div class="st-content st-pull-left">
+		<div>
+			<p class="st-main-header"><img src="../img/Icons/Kinopedia.png"/> <z>Kinopedia</z></p>
+			<div class="hs-filters">
+<form >
+								<z>il</z><select class="hs-year-select">
+					<option> 2017</option>
+					<option>2016</option>
+					<option>2015</option>
+					<option>2014</option>
+					<option>2013</option>
+					<option>2012</option>
+					<option>2011</option>
+					<option>2010</option>
+					<option>2009</option>
+					<option>2008</option>
+				</select>
+			</form>
+			<div class="hs-abc">
+			    <li><a href="#">Hamısı</a></li>
+			    <li><a href="#">0-9</a></li>
+			    <li><a href="#">A</a></li>
+			    <li><a href="#">B</a></li>
+			    <li><a href="#">C</a></li>
+			    <li><a href="#">Ç</a></li>
+			    <li><a href="#">D</a></li>
+			    <li><a href="#">E</a></li>
+			    <li><a href="#">Ə</a></li>
+			    <li><a href="#">F</a></li>
+			    <li><a href="#">G</a></li>
+			    <li><a href="#">H</a></li>
+			    <li><a href="#">X</a></li>
+			    <li><a href="#">İ</a></li>
+			    <li><a href="#">J</a></li>
+			    <li><a href="#">K</a></li>
+			    <li><a href="#">Q</a></li>
+			    <li><a href="#">L</a></li>
+			    <li><a href="#">M</a></li>
+			    <li><a href="#">N</a></li>
+			    <li><a href="#">O</a></li>
+			    <li><a href="#">Ö</a></li>
+			    <li><a href="#">P</a></li>
+			    <li><a href="#">R</a></li>
+			    <li><a href="#">S</a></li>
+			    <li><a href="#">Ş</a></li>
+			    <li><a href="#">T</a></li>
+			    <li><a href="#">U</a></li>
+			    <li><a href="#">Ü</a></li>
+			    <li><a href="#">V</a></li>
+			    <li><a href="#">Y</a></li>
+			    <li><a href="#">Z</a></li>
+			    </div>
+</div>
+			
+		</div>
+		<div class="st-greyBox">
+			<h1 class="st-main-header"><a href="#"></a></h1>
+		</div>
+	</div>
+	
+	<!-- Side Bar Starts HERE -->
+	<div class="st-sideBar st-pull-right">
+
+		 <div class="st-banner">
+			<a href="http://bit.ly/1SPKEfa" target="_blank"><img src="http://i.imgur.com/ipITnSe.png" /></a>
+		</div>
+
+
+		<div class="st-likeBox">
+			<div class="st-boxWrapper">
+        		<iframe src="http://www.facebook.com/plugins/likebox.php?id=203540399778916&amp;width=292&amp;connections=10&amp;stream=false&amp;header=false&amp;height=255" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:255px; margin:-5px 0 0 -5px;" allowTransparency="true"></iframe>
+       		</div>
+		</div>
+
+		<!-- Side Bar Top List STARTS HERE -->
+		<div class="st-list-box">
+			<h1 class="st-list-header">top list</h1>
+
+			<ul class="st-topList">
+
+				<li class="st-movie">
+					<div class="st-listImg"><span class="st-movPlace">01.</span></div>
+					<div class="st-listTitle">
+						<a href="http://kinotap.com/az/movies/1023" class="st-movTitle">Əli Baba və 7 Cırtdan
+							<span class="st-rating">xal: 10</span>
+						</a>
+					</div>					
+				</li>
+
+				<li class="st-movie">
+					<div class="st-listImg"><span class="st-movPlace">01.</span></div>
+					<div class="st-listTitle">
+						<a href="http://kinotap.com/az/movies/1023" class="st-movTitle">Əli Baba və 7 Cırtdan
+							<span class="st-rating">xal: 10</span>
+						</a>
+					</div>					
+				</li>
+
+				<li class="st-movie">
+					<div class="st-listImg"><span class="st-movPlace">01.</span></div>
+					<div class="st-listTitle">
+						<a href="http://kinotap.com/az/movies/1023" class="st-movTitle">Əli Baba və 7 Cırtdan
+							<span class="st-rating">xal: 10</span>
+						</a>
+					</div>					
+				</li>
+
+				<li class="st-movie">
+					<div class="st-listImg"><span class="st-movPlace">01.</span></div>
+					<div class="st-listTitle">
+						<a href="http://kinotap.com/az/movies/1023" class="st-movTitle">Əli Baba və 7 Cırtdan
+							<span class="st-rating">xal: 10</span>
+						</a>
+					</div>					
+				</li>
+
+				<li class="st-movie">
+					<div class="st-listImg"><span class="st-movPlace">01.</span></div>
+					<div class="st-listTitle">
+						<a href="http://kinotap.com/az/movies/1023" class="st-movTitle">Əli Baba və 7 Cırtdan
+							<span class="st-rating">xal: 10</span>
+						</a>
+					</div>					
+				</li>
+			</ul>
+		</div>
+		<!-- Side Bar Top List Ends HERE -->
+
+		<!-- Side Bar News Box STARTS HERE -->
+		<div class="st-news-box">
+			<h1 class="st-list-header">kino xəbərləri</h1>
+			<ul class="st-newsList">
+				<li class="st-newsSide">
+					<div class="st-newsBoxImg">
+						<img src="http://kinotap.com/media/news//120/tumb/pic35244197.jpg" alt="" class="pic"> 
+					</div>
+					<div class="st-newsSideContent">
+						<span class="st-newsSideDate">05.02.2015</span>
+						<a href="#" class="st-newsSideTitle">“Ay Brilliant”ın premyerasının vaxtı açıqlandı.
+						</a>
+						<span class="st-newsSidePrev">Azərbaycan kinosevərlərini yeni sürpriz gözləyir. "Belə ki, mart ayının 2-də Azərba</span>
+					</div>
+				</li>
+				<li class="st-newsSide">
+					<div class="st-newsBoxImg">
+						<img src="http://kinotap.com/media/news//120/tumb/pic35244197.jpg" alt="" class="pic"> 
+					</div>
+					<div class="st-newsSideContent">
+						<span class="st-newsSideDate">05.02.2015</span>
+						<a href="#" class="st-newsSideTitle">“Ay Brilliant”ın premyerasının vaxtı açıqlandı.
+						</a>
+						<span class="st-newsSidePrev">Azərbaycan kinosevərlərini yeni sürpriz gözləyir. "Belə ki, mart ayının 2-də Azərba</span>
+					</div>
+				</li>
+				<li class="st-newsSide">
+					<div class="st-newsBoxImg">
+						<img src="http://kinotap.com/media/news//120/tumb/pic35244197.jpg" alt="" class="pic"> 
+					</div>
+					<div class="st-newsSideContent">
+						<span class="st-newsSideDate">05.02.2015</span>
+						<a href="#" class="st-newsSideTitle">“Ay Brilliant”ın premyerasının vaxtı açıqlandı.
+						</a>
+						<span class="st-newsSidePrev">Azərbaycan kinosevərlərini yeni sürpriz gözləyir. "Belə ki, mart ayının 2-də Azərba</span>
+					</div>
+				</li>
+				<li class="st-newsSide">
+					<div class="st-newsBoxImg">
+						<img src="http://kinotap.com/media/news//120/tumb/pic35244197.jpg" alt="" class="pic"> 
+					</div>
+					<div class="st-newsSideContent">
+						<span class="st-newsSideDate">05.02.2015</span>
+						<a href="#" class="st-newsSideTitle">“Ay Brilliant”ın premyerasının vaxtı açıqlandı.
+						</a>
+						<span class="st-newsSidePrev">Azərbaycan kinosevərlərini yeni sürpriz gözləyir. "Belə ki, mart ayının 2-də Azərba</span>
+					</div>
+				</li>
+				<li class="st-newsSide">
+					<div class="st-newsBoxImg">
+						<img src="http://kinotap.com/media/news//120/tumb/pic35244197.jpg" alt="" class="pic"> 
+					</div>
+					<div class="st-newsSideContent">
+						<span class="st-newsSideDate">05.02.2015</span>
+						<a href="#" class="st-newsSideTitle">“Ay Brilliant”ın premyerasının vaxtı açıqlandı.
+						</a>
+						<span class="st-newsSidePrev">Azərbaycan kinosevərlərini yeni sürpriz gözləyir. "Belə ki, mart ayının 2-də Azərba</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<!-- Side Bar News Box ENDS HERE -->
+
+	</div>
+	<!-- Side Bar Ends HERE -->		
+
+</div>
+<!-- Middle Section Ends HERE -->
+
+
+<div class="footer">
+	<!-- <div class="row"> -->
+		<div class="col-md-12 footer">
+			<div class="col-md-12 footerUp"></div>
+			<div class="col-md-12 footerDown"></div>
+		</div>
+	<!-- </div> -->
+</div>
+	
+
+
+
+
+
+
+</div>
+</div>
+</body>
+</html>
